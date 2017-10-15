07222f3f88c86796bbd1b4a7eff2bd755e3f8308
elvis <elvis@lnmpy.com>
2017-10-15 23:49:08 +0800

    update login page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -4,7 +4,9 @@
     img(src='/static/logo-toby.svg')
     mt-field(placeholder="Email", v-model="email")
     mt-field(placeholder="Password", type="password", v-model="password")
-    mt-button(type="primary", size='large', @click='login()') Login
+    mt-button(type="primary", size='large', @click='!loading && login()')
+      span(v-if='!loading') Login
+      mt-spinner(type="fading-circle", v-if='loading', color="white", style='display: table-cell;')
 
 </template>
 
@@ -17,14 +19,17 @@ export default {
     return {
       email: '',
       password: '',
+      loading: false,
     };
   },
   methods: {
     login() {
       delete Vue.http.headers.common.Authorization;
+      this.loading = true;
       this.$http.post('users/login',
         { email: this.email, password: this.password })
         .then((resp) => {
+          this.loading = false;
           localStorage.setItem('AuthToken', resp.body.token);
           Vue.http.headers.common.Authorization = resp.body.token;
           this.$router.push('/');
@@ -42,9 +47,19 @@ export default {
   align-items: center;
   .login-form {
     margin-top: 30vh;
+    display: flex;
+    flex-direction: column;
+    max-width: 80%;
+    width: 30em;
     img {
       width: 10rem;
+      margin: 30px auto;
     }
+  }
+}
+.login-view {
+  .mint-button {
+    margin-top: 10px;
   }
 }
 </style>
