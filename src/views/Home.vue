dd66beaa179f0459c4eefa7491cf198309d1475c
elvis <elvis@lnmpy.com>
2017-10-18 11:37:04 +0800

    update collapse

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,11 +3,14 @@
   .content(v-if='!syncingAtFirst')
     mu-refresh-control(:refreshing="syncing", :trigger="trigger", @refresh="syncStates")
     mu-list(v-for='list of lists', key='id')
-      mu-sub-header {{ list.title }}
-      mu-list-item(v-for='card of list.cards', key='id', :title='card.title', :href='card.url', target='_blank')
-        mu-avatar(:src='card.favIconUrl', slot='leftAvatar', backgroundColor='white', :size='40')
-        .description(slot='describe') {{ card.description }}
-        mu-divider
+      mu-sub-header
+        span {{ list.title }}
+        mu-icon-button(:icon='list.expanded?"keyboard_arrow_up":"keyboard_arrow_down"', @click='list.expanded=!list.expanded')
+      .cards(:style='{maxHeight: (list.expanded?"200vh":0), overflowY: (list.expanded?"auto":"hidden")}')
+        mu-list-item(v-for='card of list.cards', key='id', :title='card.title | truncate(20) ', :href='card.url', target='_blank')
+          mu-avatar(:src='card.favIconUrl', slot='leftAvatar', backgroundColor='white', :size='40')
+          .description(slot='describe') {{ card.description }}
+          mu-divider
     mu-float-button(icon='add')
 
   mu-circular-progress(v-if='syncingAtFirst', :size='60', :strokeWidth='5')
@@ -64,14 +67,26 @@ export default {
   }
   .mu-float-button {
     position: fixed;
-    top: 90vh;
-    right: 10px;
+    bottom: 3vh;
+    right: 3vh;
   }
   .mu-circular-progress {
     position: fixed;
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
+  }
+  .mu-sub-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 56px;
+    padding: 0 24px;
+    background-color: #fbfbfb;
+    cursor: pointer;
+  }
+  .cards {
+    transition: max-height .8s cubic-bezier(.445,.05,.55,.95);
   }
 }
 </style>
