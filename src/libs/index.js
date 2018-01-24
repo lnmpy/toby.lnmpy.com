146e87d9f3566f4e8e7ed0ac091835bf76144a56
elvis <elvis@lnmpy.com>
2018-01-24 14:56:45 +0800

    update the index.js

--- src/libs/index.js
+++ src/libs/index.js
@@ -1,5 +1,9 @@
-import './http-interceptors';
-import './router-interceptors';
-import './raven';
-import './filters';
-import './muse';
+const files = require.context('.', false, /\.js$/);
+const modules = {};
+
+files.keys().forEach((key) => {
+  if (key === './index.js') return;
+  modules[key.replace(/(\.\/|\.js)/g, '')] = files(key).default;
+});
+
+export default modules;
